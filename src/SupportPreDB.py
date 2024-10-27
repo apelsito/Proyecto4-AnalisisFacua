3725da8f9d1dd8650076274d2d0f059d9a2b49b8
apelsito <mrappletastico@gmail.com>
2024-10-27 23:36:33 +0100

    Docstrings y comentarios añadidos

--- src/SupportPreDB.py
+++ src/SupportPreDB.py
@@ -1,35 +1,52 @@
-# Importamos las librerías que necesitamos
+# Importación de librerías necesarias para el proyecto
 
-# Librerías para automatización de navegadores (Selenium)
+# Automatización de navegadores (Selenium)
 # -----------------------------------------------------------------------
-from selenium import webdriver                   # Control automático del navegador para realizar scraping web
-
-from selenium.webdriver.chrome.service import Service  # Maneja el servicio de ChromeDriver
-from selenium.webdriver.common.by import By      # Para seleccionar elementos en la página web (DOM)
+from selenium import webdriver                      # Control automático del navegador para realizar scraping web
+from selenium.webdriver.chrome.service import Service  # Manejo del servicio de ChromeDriver
+from selenium.webdriver.common.by import By         # Selección de elementos en el DOM
 from selenium.webdriver.chrome.options import Options  # Configuración de opciones del navegador (headless mode, etc.)
-import time                                      # Para gestionar pausas y temporización
+import time                                         # Para gestionar pausas y temporización
 
-# Librerías para extracción de datos y scraping
+# Extracción de datos y scraping
 # -----------------------------------------------------------------------
-from bs4 import BeautifulSoup  # Para analizar y extraer datos de HTML y XML (scraping web)
+from bs4 import BeautifulSoup                       # Análisis y extracción de datos de HTML y XML (scraping web)
 
-# Librerías para tratamiento y manipulación de datos
+# Tratamiento y manipulación de datos
 # -----------------------------------------------------------------------
-import pandas as pd             # Para manipulación de estructuras de datos como DataFrames
-import numpy as np              # Para cálculos numéricos y manejo de arrays
-import datetime                 # Para manipulación de fechas y tiempos
+import pandas as pd                                 # Manipulación de estructuras de datos como DataFrames
+import numpy as np                                  # Cálculos numéricos y manejo de arrays
+import datetime                                     # Manipulación de fechas y tiempos
+
+# Visualización de progreso
 # -----------------------------------------------------------------------
-# Librerías para visualización de progreso
-# -----------------------------------------------------------------------
-from tqdm import tqdm  # Para crear barras de progreso durante los bucles
+from tqdm import tqdm                               # Creación de barras de progreso durante bucles
 
 # Manejo de tiempo y solicitudes con espera
 # -----------------------------------------------------------------------
-from time import sleep          # Para pausar la ejecución del código por un tiempo determinado
-import random                   # Para generar valores aleatorios, útil para espaciar solicitudes y evitar bloqueos
-
+from time import sleep                              # Pausas en la ejecución del código
+import random                                       # Generación de valores aleatorios, útil para espaciar solicitudes y evitar bloqueos
 
 def crear_df_unicos(df, nombre_columna):
+    """
+    Crea un DataFrame con valores únicos de una columna específica y un índice numerado a partir de 1.
+
+    Args:
+        df (pandas.DataFrame): DataFrame de entrada que contiene la columna de interés.
+        nombre_columna (str): Nombre de la columna cuyos valores únicos se extraerán.
+
+    Returns:
+        pandas.DataFrame: DataFrame que contiene una columna con los valores únicos y una columna de índice numerado.
+
+    Example:
+        >>> import pandas as pd
+        >>> df = pd.DataFrame({'col1': [1, 2, 2, 3], 'col2': ['A', 'B', 'B', 'C']})
+        >>> crear_df_unicos(df, 'col2')
+           index col2
+        0      1    A
+        1      2    B
+        2      3    C
+    """
     columna = nombre_columna
     lista_unicos = df[columna].unique().tolist()
     df_unicos = pd.DataFrame({
@@ -44,6 +61,24 @@ def crear_df_unicos(df, nombre_columna):
     return df_unicos
 
 def crear_dictio(df,columna):
+    """
+    Crea un diccionario a partir de un DataFrame, donde las claves son valores únicos de una columna específica,
+    y los valores son los primeros índices asociados a cada clave en el DataFrame.
+
+    Args:
+        df (pandas.DataFrame): DataFrame de entrada que contiene la columna de interés.
+        columna (str): Nombre de la columna cuyos valores únicos se usarán como claves en el diccionario.
+
+    Returns:
+        dict: Diccionario donde las claves son los valores únicos de la columna especificada, y los valores son 
+              los primeros índices asociados a cada clave en el DataFrame.
+
+    Example:
+        >>> import pandas as pd
+        >>> df = pd.DataFrame({'index': [1, 2, 3, 4], 'columna': ['A', 'B', 'A', 'C']})
+        >>> crear_dictio(df, 'columna')
+        {'A': 1, 'B': 2, 'C': 4}
+    """
     dictio = {}
     datos = df.groupby(columna)["index"].first()
     indices = list(datos.index)
@@ -54,6 +89,30 @@ def crear_dictio(df,columna):
     return dictio
 
 def crear_df_historico(df_historicos,df_supermercado,df_categoria,df_producto):
+    """
+    Modifica el DataFrame `df_historicos`, reemplazando los nombres de supermercados, categorías y productos
+    con sus correspondientes índices, utilizando diccionarios generados a partir de DataFrames de referencia.
+
+    Args:
+        df_historicos (pandas.DataFrame): DataFrame que contiene los datos históricos con columnas de nombres
+                                          de supermercados, categorías y productos.
+        df_supermercado (pandas.DataFrame): DataFrame que contiene los nombres de los supermercados y sus índices.
+        df_categoria (pandas.DataFrame): DataFrame que contiene los nombres de las categorías y sus índices.
+        df_producto (pandas.DataFrame): DataFrame que contiene los nombres de los productos y sus índices.
+
+    Returns:
+        pandas.DataFrame: DataFrame `df_historicos` modificado, con nombres reemplazados por sus índices correspondientes.
+
+    Example:
+        >>> df_historicos = pd.DataFrame({'supermercado': ['Super1', 'Super2'], 'categoria': ['Cat1', 'Cat2'], 'producto': ['Prod1', 'Prod2']})
+        >>> df_supermercado = pd.DataFrame({'index': [1, 2], 'supermercado': ['Super1', 'Super2']})
+        >>> df_categoria = pd.DataFrame({'index': [1, 2], 'categoria': ['Cat1', 'Cat2']})
+        >>> df_producto = pd.DataFrame({'index': [1, 2], 'producto': ['Prod1', 'Prod2']})
+        >>> crear_df_historico(df_historicos, df_supermercado, df_categoria, df_producto)
+           supermercado  categoria  producto
+        0            1          1         1
+        1            2          2         2
+    """
     # Creamos los diccionarios
     dictio_supermercados = crear_dictio(df_supermercado,"supermercado")
     dictio_categorias = crear_dictio(df_categoria,"categoria")
--- src/SupportScraping.py
+++ src/SupportScraping.py
@@ -1,38 +1,54 @@
-# Importamos las librerías que necesitamos
+# Importación de librerías necesarias para el proyecto
 
-# Librerías para automatización de navegadores (Selenium)
+# Automatización de navegadores (Selenium)
 # -----------------------------------------------------------------------
-from selenium import webdriver                   # Control automático del navegador para realizar scraping web
+from selenium import webdriver                      # Control automático del navegador para scraping web
+from selenium.webdriver.chrome.service import Service  # Manejo del servicio de ChromeDriver
+from selenium.webdriver.common.by import By         # Selección de elementos en el DOM
+from selenium.webdriver.chrome.options import Options  # Configuración de opciones de navegador (headless mode, etc.)
+import time                                         # Para gestionar pausas y temporización
 
-from selenium.webdriver.chrome.service import Service  # Maneja el servicio de ChromeDriver
-from selenium.webdriver.common.by import By      # Para seleccionar elementos en la página web (DOM)
-from selenium.webdriver.chrome.options import Options  # Configuración de opciones del navegador (headless mode, etc.)
-import time                                      # Para gestionar pausas y temporización
+# Extracción de datos y scraping
+# -----------------------------------------------------------------------
+from bs4 import BeautifulSoup                       # Análisis y extracción de datos de HTML y XML (scraping web)
+import requests                                     # Manejo de solicitudes HTTP para obtener datos de la web
 
-# Librerías para extracción de datos y scraping
+# Visualización de progreso
 # -----------------------------------------------------------------------
-from bs4 import BeautifulSoup  # Para analizar y extraer datos de HTML y XML (scraping web)
+from tqdm import tqdm                               # Creación de barras de progreso durante bucles
 
+# Tratamiento y manipulación de datos
 # -----------------------------------------------------------------------
-# Librerías para visualización de progreso
-# -----------------------------------------------------------------------
-from tqdm import tqdm  # Para crear barras de progreso durante los bucles
-
-# Librerías para tratamiento y manipulación de datos
-# -----------------------------------------------------------------------
-import pandas as pd             # Para manipulación de estructuras de datos como DataFrames
-import numpy as np              # Para cálculos numéricos y manejo de arrays
-import datetime                 # Para manipulación de fechas y tiempos
+import pandas as pd                                 # Manipulación de estructuras de datos como DataFrames
+import numpy as np                                  # Cálculos numéricos y manejo de arrays
+import datetime                                     # Manipulación de fechas y tiempos
 
 # Manejo de tiempo y solicitudes con espera
 # -----------------------------------------------------------------------
-from time import sleep          # Para pausar la ejecución del código por un tiempo determinado
-import random                   # Para generar valores aleatorios, útil para espaciar solicitudes y evitar bloqueos
-import requests                 # Repetido (puedes eliminar este segundo import de requests)
+from time import sleep                              # Pausas en la ejecución del código
+import random                                       # Generación de valores aleatorios, útil para espaciar solicitudes y evitar bloqueos
 
 
 def obtener_url_supermercados(xpath):
-    
+    """
+    Obtiene la URL de una página de supermercados de FACUA tras interactuar con elementos de la interfaz.
+
+    Args:
+        xpath (str): Cadena con el XPATH del elemento a hacer clic, una vez que la página ha sido cargada.
+
+    Returns:
+        str: URL de la página actual después de hacer clic en el elemento especificado.
+
+    Example:
+        >>> xpath = '//*[@id="elemento_supermercado"]'
+        >>> url = obtener_url_supermercados(xpath)
+        >>> print(url)
+        
+    Notes:
+        - Requiere tener instalado y configurado Selenium junto con el driver de Chrome.
+        - La ventana se maximiza y se realiza un scroll antes de hacer clic en el elemento.
+        - Asegúrate de ejecutar esta función una sola vez para evitar múltiples instancias del navegador.
+    """
     driver = webdriver.Chrome() #Para Abrir chrome solo ejecutar once
     url_facua= "https://super.facua.org/"
     driver.get(url_facua)
@@ -50,6 +66,26 @@ def obtener_url_supermercados(xpath):
     return url_obtenida
 
 def obtener_url_productos_por_supermercado(url):
+    """
+    Obtiene una lista de URLs de productos por supermercado desde la página de FACUA, navegando e interactuando
+    con elementos específicos de la interfaz para cada categoría de productos.
+
+    Args:
+        url (str): URL inicial de la página del supermercado en FACUA desde donde se obtendrán las URLs de productos.
+
+    Returns:
+        list of str: Lista de URLs correspondientes a diferentes categorías de productos en el supermercado.
+
+    Example:
+        >>> url = "https://super.facua.org/supermercado"
+        >>> urls_productos = obtener_url_productos_por_supermercado(url)
+        >>> print(urls_productos)
+        
+    Notes:
+        - Requiere tener instalado y configurado Selenium junto con el driver de Chrome.
+        - La función realiza varias interacciones en la página, incluyendo clics, desplazamiento y navegaciones hacia atrás.
+        - Cada URL obtenida representa una categoría de productos en el supermercado especificado.
+    """
     urls = []
     driver = webdriver.Chrome() #Para Abrir chrome solo ejecutar once
     driver.get(url)
@@ -86,6 +122,32 @@ def obtener_url_productos_por_supermercado(url):
 
 
 def obtener_urls_por_super_producto_y_categoria(url1,url2,url3):
+    """
+    Obtiene una lista de URLs correspondientes a distintas categorías de productos en tres páginas de supermercados, 
+    y devuelve un DataFrame con el nombre del supermercado y las URLs obtenidas.
+
+    Args:
+        url1 (str): URL de la primera página del supermercado.
+        url2 (str): URL de la segunda página del supermercado.
+        url3 (str): URL de la tercera página del supermercado.
+
+    Returns:
+        pandas.DataFrame: DataFrame que contiene dos columnas:
+                          - "supermercado": Nombre del supermercado, extraído de la primera URL.
+                          - "URL": Lista de URLs correspondientes a cada categoría de productos en las páginas.
+
+    Example:
+        >>> url1 = "https://super.facua.org/supermercado1"
+        >>> url2 = "https://super.facua.org/supermercado2"
+        >>> url3 = "https://super.facua.org/supermercado3"
+        >>> df_urls = obtener_urls_por_super_producto_y_categoria(url1, url2, url3)
+        >>> print(df_urls)
+        
+    Notes:
+        - Requiere tener instalado y configurado Selenium junto con el driver de Chrome.
+        - La función realiza varias interacciones en las páginas, incluyendo clics, desplazamiento y navegación.
+        - La ventana del navegador se maximiza y se ejecuta un desplazamiento hacia abajo en cada página para alcanzar elementos interactivos.
+    """
     urls = []
     supermercado = url1.split("/")[3]
     driver = webdriver.Chrome() #Para Abrir chrome solo ejecutar once
@@ -174,6 +236,29 @@ def obtener_urls_por_super_producto_y_categoria(url1,url2,url3):
     return df
 
 def obtener_href(url):
+    """
+    Extrae enlaces y nombres de productos desde la página de un supermercado en FACUA y los organiza en un DataFrame.
+
+    Args:
+        url (str): URL de la página del supermercado en FACUA, de la cual se extraerán los datos de productos.
+
+    Returns:
+        pandas.DataFrame: DataFrame con las siguientes columnas:
+                          - "supermercado": Nombre del supermercado, extraído de la URL.
+                          - "categoria": Categoría de productos, extraída de la URL.
+                          - "Producto": Nombre del producto extraído de la página.
+                          - "URL": Enlace al producto correspondiente.
+
+    Example:
+        >>> url = "https://super.facua.org/supermercado/categoria/productos"
+        >>> df_productos = obtener_href(url)
+        >>> print(df_productos)
+        
+    Notes:
+        - Requiere las librerías `requests` y `BeautifulSoup` para obtener y analizar el contenido de la página.
+        - La función configura encabezados para la solicitud, simulando un navegador Chrome y estableciendo el idioma en español.
+        - Los nombres y URLs de los productos se extraen basándose en etiquetas HTML específicas y clases de la página de FACUA.
+    """
     headers = {
             # Decirle al server lenguaje español
             'accept-language': 'es', 
@@ -209,11 +294,51 @@ def obtener_href(url):
     return df
 
 def obtener_href_productos(df_super):
+    """
+    Obtiene una lista de DataFrames con información de productos desde múltiples URLs de supermercados.
+
+    Args:
+        df_super (pandas.DataFrame): DataFrame que contiene una columna "URL" con enlaces a páginas de supermercados en FACUA.
+
+    Returns:
+        list of pandas.DataFrame: Lista de DataFrames, donde cada DataFrame contiene información de productos 
+                                  (supermercado, categoría, nombre de producto, y URL) extraída de cada URL en `df_super`.
+
+    Example:
+        >>> df_super = pd.DataFrame({"URL": ["https://super.facua.org/super1", "https://super.facua.org/super2"]})
+        >>> lista_dfs = obtener_href_productos(df_super)
+        >>> for df in lista_dfs:
+        >>>     print(df.head())
+        
+    Notes:
+        - Requiere la función `obtener_href` para extraer información de productos de cada URL.
+        - Cada URL en `df_super` es procesada y se obtiene un DataFrame con información de productos de esa página.
+    """
     lista_urls = df_super["URL"].to_list()
     lista_dfs = [obtener_href(url) for url in lista_urls]
     return lista_dfs
 
 def crear_df_rn(supermercado,categoria,producto,url):
+    """
+    Extrae información histórica de precios de productos desde una página de FACUA y organiza los datos en un DataFrame.
+
+    Args:
+        supermercado (str): Nombre del supermercado.
+        categoria (str): Categoría del producto.
+        producto (str): Nombre del producto.
+        url (str): URL de la página donde se encuentran los datos históricos de precios del producto.
+
+    Returns:
+        pandas.DataFrame: DataFrame con columnas para supermercado, categoría, producto, fecha, precio y variación de precio.
+
+    Example:
+        >>> df_historico = crear_df_rn("Super1", "Lácteos", "Leche", "https://super.facua.org/producto1")
+        >>> print(df_historico.head())
+
+    Notes:
+        - Requiere las librerías `requests` y `BeautifulSoup` para obtener y analizar el contenido de la página.
+        - La función extrae las columnas directamente desde la tabla en la página, por lo que es importante que la estructura de la página no cambie.
+    """
     headers = {
                 # Decirle al server lenguaje español
                 'accept-language': 'es', 
@@ -255,6 +380,32 @@ def crear_df_rn(supermercado,categoria,producto,url):
     return df_result
 
 def crear_df_productos_con_historico(df_que_entra):
+    """
+    Crea una lista de DataFrames con datos históricos de precios de productos, utilizando la información de un DataFrame de entrada.
+
+    Args:
+        df_que_entra (pandas.DataFrame): DataFrame que contiene las columnas "supermercado", "categoria", "Producto" y "URL",
+                                         con la información necesaria para extraer los datos de precios históricos.
+
+    Returns:
+        list of pandas.DataFrame: Lista de DataFrames, donde cada DataFrame contiene los datos históricos de precios de un producto,
+                                  incluyendo supermercado, categoría, producto, fecha, precio y variación.
+
+    Example:
+        >>> df_productos = pd.DataFrame({
+                "supermercado": ["Super1", "Super2"],
+                "categoria": ["Lácteos", "Cereales"],
+                "Producto": ["Leche", "Cereal"],
+                "URL": ["https://super.facua.org/producto1", "https://super.facua.org/producto2"]
+            })
+        >>> historicos = crear_df_productos_con_historico(df_productos)
+        >>> for df in historicos:
+        >>>     print(df.head())
+
+    Notes:
+        - Utiliza `crear_df_rn` para extraer datos históricos desde cada URL en `df_que_entra`.
+        - La función requiere `tqdm` para mostrar una barra de progreso durante el procesamiento de múltiples URLs.
+    """
     lista_supermercado = df_que_entra["supermercado"].to_list()
     lista_categoria = df_que_entra["categoria"].to_list()
     lista_productos = df_que_entra["Producto"].to_list()
